90861373bc719d1b8ab68fa9bef94a879fed28cf
rufus <rufusaddanki777@gmail.com>
2025-06-18 01:25:22 +0530

    upadted .env database and index.js

--- db.js
+++ db.js
@@ -2,15 +2,18 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,  // Render DB name
-  process.env.DB_USER,  // Render DB User
-  process.env.DB_PASSWORD, //Render password
   {
     host: process.env.DB_HOST, //Render host
     port: process.env.DB_PORT || 3306,
     dialect: process.env.DB_DIALECT || "postgres",
     logging: false
-  }
+  },
+
+  process.env.DB_NAME,  // Render DB name
+  process.env.DB_PASSWORD,  // Render DB password
+  process.env.DB_USER, //Render DB USER
+
+
 );
 
 
