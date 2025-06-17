ed8df732780ea974509b9083050ff224155a4226
rufus <rufusaddanki777@gmail.com>
2025-06-18 00:34:21 +0530

    Updated .env and db.jd routes

--- db.js
+++ db.js
@@ -7,14 +7,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD, //Render password
   {
     host: process.env.DB_HOST, //Render host
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-    
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || "postgres",
     logging: false
   }
 );
