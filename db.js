165cfe5794304dc71bfd5283d7151c94ff2b69b8
rufus <rufusaddanki777@gmail.com>
2025-06-19 02:30:22 +0530

    updated db.js

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST, //Render host
     port: process.env.DB_PORT,
-    dialect: "postgres",
+    dialect: process.env.DB_DIALECT || "postgres",
     protocol: "postgres",
     logging: false
   }
