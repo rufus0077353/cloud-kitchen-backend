3959973ed060d6e9e15ca32d36bdfcc822d8be55
rufus <rufusaddanki777@gmail.com>
2025-07-14 00:48:23 +0530

    Updated routes

--- models/User.js
+++ models/User.js
@@ -1,35 +1,23 @@
+
 const bcrypt = require("bcrypt");
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    role: {
-      type: DataTypes.STRING,
-      defaultValue: "user", // very important for role-based access
-    }
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, defaultValue: "user" }
   }, {
     tableName: 'users',
     timestamps: true,
-    freezeTableName: true 
+    freezeTableName: true
   });
 
-  // Hash password before saving
   User.beforeCreate(async (user) => {
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
   });
+
 
   // Instance method for password validation
   User.prototype.validPassword = async function (inputPassword) {
