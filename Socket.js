16a4842cbe512138f59da52bc4a48e1728c96852
rufus <rufusaddanki777@gmail.com>
2025-09-13 17:27:37 +0530

    updated

--- Socket.js
+++ Socket.js
@@ -1,3 +1,4 @@
+
 // socket.js
 const { Server } = require("socket.io");
 
@@ -5,20 +6,30 @@ let io;
 
 function initSocket(server, origins) {
   io = new Server(server, {
+    path: "/socket.io",
     cors: {
-      origin: origins,
+      origin: origins, // array of allowed frontends
       methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
       credentials: true,
     },
+    pingInterval: 25000,
+    pingTimeout: 60000,
+    allowEIO3: true, // tolerate older client versions
   });
 
-  // Rooms: vendor & user
   io.on("connection", (socket) => {
-    socket.on("vendor:join", (vendorId) => {
-      if (vendorId) socket.join(`vendor:${vendorId}`);
+    console.log("🔌 socket connected", socket.id);
+
+    // join “rooms”
+    socket.on("vendor:join", (vendorId) => vendorId && socket.join(`vendor:${vendorId}`));
+    socket.on("user:join",   (userId)   => userId   && socket.join(`user:${userId}`));
+
+    socket.on("auth:refresh", () => {
+      // add token checks here if you’d like
     });
-    socket.on("user:join", (userId) => {
-      if (userId) socket.join(`user:${userId}`);
+
+    socket.on("disconnect", (reason) => {
+      console.log("🔌 socket disconnected:", reason);
     });
   });
 
@@ -40,9 +51,4 @@ function emitToUser(userId, event, payload) {
   io.to(`user:${userId}`).emit(event, payload);
 }
 
-module.exports = {
-  initSocket,
-  getIO,
-  emitToVendor,
-  emitToUser,
-};
+module.exports = { initSocket, getIO, emitToVendor, emitToUser };
