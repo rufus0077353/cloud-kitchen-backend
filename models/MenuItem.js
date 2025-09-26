dcd3eef6b1e0b1df81a5ca0a4d0333005d88f396
rufus <rufusaddanki777@gmail.com>
2025-09-26 16:09:03 +0530

    updated pages

--- models/MenuItem.js
+++ models/MenuItem.js
@@ -1,16 +1,22 @@
+// models/MenuItem.js
 module.exports = (sequelize, DataTypes) => {
-  const MenuItem = sequelize.define("MenuItem", {
-    name:        { type: DataTypes.STRING, allowNull: false },
-    price:       { type: DataTypes.FLOAT,  allowNull: false },
-    description: { type: DataTypes.STRING },
-    isAvailable: { type: DataTypes.BOOLEAN, defaultValue: true },
-    VendorId:    { type: DataTypes.INTEGER, allowNull: false },
-    imageURL:   { type: DataTypes.STRING, allowNull: true, validate: { isUrl: true } 
-  },
-  }, {
-    tableName: "menu_items",
-    timestamps: true,
-  });
+  const MenuItem = sequelize.define(
+    "MenuItem",
+    {
+      name:        { type: DataTypes.STRING, allowNull: false },
+      price:       { type: DataTypes.FLOAT,  allowNull: false },
+      description: { type: DataTypes.STRING, allowNull: true },
+      isAvailable: { type: DataTypes.BOOLEAN, defaultValue: true },
+      VendorId:    { type: DataTypes.INTEGER, allowNull: false },
+
+      // 👇 canonical key expected by your routes & frontend
+      imageUrl:    { type: DataTypes.STRING(1024), allowNull: true, validate: { isUrl: true } },
+    },
+    {
+      tableName: "menu_items",
+      timestamps: true,
+    }
+  );
 
   return MenuItem;
 };
